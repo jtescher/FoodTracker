bc501fad1f02daadd0570f6c343ff3f6598f267d
Julian Tescher <jatescher@gmail.com>
2015-12-30 16:19:36 -0800

    First view controller properties and actions

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -10,10 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var mealNameTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-}
+    // MARK: Actions
+    @IBAction func setDefaultLabelText(sender: AnyObject) {
+        mealNameLabel.text = "Default Text"
+    }
 
+}
