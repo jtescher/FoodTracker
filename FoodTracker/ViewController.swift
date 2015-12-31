19ec626fed3939e7256e30d0846e0594273b0a82
Julian Tescher <jatescher@gmail.com>
2015-12-30 17:43:29 -0800

    Dynamic label update

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,18 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+
 
     // MARK: Properties
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var mealNameTextField: UITextField!
 
+
+    // MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Handle the text field's user input through delegate callbacks.
+        mealNameTextField.delegate = self
     }
 
+
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+
+        return true
+    }
+
+    func textFieldDidEndEditing(textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+
+
     // MARK: Actions
+
     @IBAction func setDefaultLabelText(sender: AnyObject) {
         mealNameLabel.text = "Default Text"
     }
