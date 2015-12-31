1a6c692329420fa80afb0f3d3de448080a824451
Julian Tescher <jatescher@gmail.com>
2015-12-30 19:49:14 -0800

    Add custom button

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,34 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Julian Tescher on 12/30/15.
+//  Copyright © 2015 Julian Tescher. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+
+    // MARK: Initialization
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        // Add temp button to see example of subview
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+        button.backgroundColor = UIColor.redColor()
+        button.addTarget(self, action: "ratingButtonTapped:", forControlEvents: .TouchDown)
+        addSubview(button)
+    }
+    override func intrinsicContentSize() -> CGSize {
+        return CGSize(width: 240, height: 44)
+    }
+
+
+    // MARK: Button Action
+    func ratingButtonTapped(button: UIButton) {
+        print("Button pressed 👍")
+    }
+
+}
