8a2a7d87ba5e1154e52348aa9016978c8e025f5e
Julian Tescher <jatescher@gmail.com>
2016-01-01 10:59:56 -0800

    Add first unit test

--- FoodTrackerTests/FoodTrackerTests.swift
+++ FoodTrackerTests/FoodTrackerTests.swift
@@ -10,27 +10,19 @@ import XCTest
 @testable import FoodTracker
 
 class FoodTrackerTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    // MARK: FoodTracker Tests
+    func testMealInitialization() {
+        // Success case
+        let meal = Meal(name: "Newest Meal", photo: nil, rating: 5)
+        XCTAssertNotNil(meal)
+
+        // Failure cases
+        let mealWithoutValidName = Meal(name: "", photo: nil, rating: 5)
+        XCTAssertNil(mealWithoutValidName, "Empty name is invalid")
+
+        let mealWithoutValidRating = Meal(name: "", photo: nil, rating: 5)
+        XCTAssertNil(mealWithoutValidRating, "Negative rating is invalid")
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+
 }
